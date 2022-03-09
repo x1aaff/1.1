6b8ab6f821d432b2e8f2e6283c4f1a6d4c3e2d1e
x1aff <x1affunit@outlook.com>
2022-03-09 19:24:05 +0300

    2

---
+++ 1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
