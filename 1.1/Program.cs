9083b0c0567a01d72bc19cbcb680f5dbe1ec3bc8
x1aff <x1affunit@outlook.com>
2022-03-09 19:17:38 +0300

    Add project files.

---
+++ 1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
