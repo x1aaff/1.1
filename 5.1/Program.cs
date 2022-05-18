af843699b6196497dae88140eaf44611b5d35857
x1aff <x1affunit@outlook.com>
2022-05-18 18:44:18 +0300

    5

---
+++ 5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string[] SplitText(string text)
+            {
+                return text.Split(' ');
+            }
+
+            static void PrintWords (string[] words)
+            {
+                foreach (string word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+            PrintWords(SplitText(Console.ReadLine()));
+        }
+    }
+}
---
+++ 5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string[] SplitReversedText(string text)
+            {
+                string[] newText = text.Split(' ');
+                Array.Reverse(newText);
+                return newText;
+            }
+
+            static string ReverseWords (string phrase)
+            {
+                string invertedText = "";
+                foreach (string word in SplitReversedText(phrase))
+                {
+                    invertedText += word + " ";
+                }
+                return invertedText.Trim();
+            }
+
+            Console.WriteLine(ReverseWords(Console.ReadLine()));
+        }
+    }
+}
